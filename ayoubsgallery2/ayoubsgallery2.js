e6f27ffe24f6e867c0402d4d19ba19fbc2753a8f
Ayoub Markhouss <ayoub.markhouss@gmail.com>
2023-05-09 15:24:41 +0100

    some changes in html css js files

--- ayoubsgallery2/ayoubsgallery2.js
+++ ayoubsgallery2/ayoubsgallery2.js
@@ -1,5 +1,6 @@
 const showBtn = document.querySelector('.navBtn');
 const topNav = document.querySelector('.top-nav');
+const topNav2 = document.querySelector('.top-nav2');
 
 showBtn.addEventListener('click', function(){
     if(topNav.classList.contains('showNav')){
@@ -7,6 +8,17 @@ showBtn.addEventListener('click', function(){
         showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
     } else {
         topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+    }
+});
+
+/***top nav2 */
+showBtn.addEventListener('click', function(){
+    if(topNav2.classList.contains('showNav')){
+        topNav2.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+    } else {
+        topNav2.classList.add('showNav');
         showBtn.innerHTML = '<i class = "fas fa-times"></i>';
     }
 });
@@ -19,3 +31,4 @@ var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
 function logorefresh(){
     location.reload();
 }
+
